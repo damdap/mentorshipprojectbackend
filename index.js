86cbdae30eed1ab246402720e9d4c834511dd927
Damilola Bamgbose <damilolabamgbose@gmail.com>
2025-07-07 09:48:18 +0100

    fix: use Render port from env

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get("/", (req, res) => {
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
